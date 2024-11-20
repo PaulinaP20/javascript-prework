c306e52b92bcee9edcd8f298277c9e481647b043
Paulina Partyka <paulinapartyka2@gmail.com>
2024-11-20 20:19:41 +0100

    add main variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-var computerMove;
-computerMove="kamien"
 
-var playerMove;
-playerMove="papier"
+var computerMove, randomMove;
+randomNumber=Math.floor(Math.random() *3+1);
+console.log('wylosowana liczba to:'+ randomNumber);
+if (randomNumber=='1'){
+    computerMove='kamien';
+}   else if (randomNumber=='2') {
+    computerMove='papier';
+}   else if (randomNumber=='3'){
+    computerMove='nozyce'
+}
+printMessage('Moj ruch: ' + computerMove)
 
-printMessage('Zagrałem '+ computerMove + " ! Jesli twoj truch to "+ playerMove + " wygrywasz!")
+
+var playerMove, playerInput;
+playerInput=prompt('wybierz swoj ruch:1:kamien, 2:papier, 3:nozyce.');
+console.log('Wpisana odpowiedz to:'+ playerInput);
+if (playerInput=='1') {
+    playerMove='kamien';
+}   else if (playerInput=='2') {
+    playerMove='papier';
+}   else if (playerInput=='3') {
+    playerMove='nozyce';
+}   else {
+    playerMove='bledny wybor';
+}
+printMessage('Twoj ruch: '+ playerMove)
